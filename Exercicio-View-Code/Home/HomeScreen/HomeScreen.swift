19b5bb5c42d40219992e89822433b5ef38ed5215
CelioFsDev <celioferreira.dev@gmail.com>
2023-03-22 10:01:51 -0300

    Config View code project initial

---
+++ Exercicio-View-Code/Home/HomeScreen/HomeScreen.swift
@@ -0,0 +1,14 @@
+//
+//  HomeScreen.swift
+//  Exercicio-View-Code
+//
+//  Created by Celio Ferreira on 22/03/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+
+
+}
---
+++ Exercicio-View-Code/Home/HomeViewModel/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Exercicio-View-Code
+//
+//  Created by Celio Ferreira on 22/03/23.
+//
+
+import Foundation
